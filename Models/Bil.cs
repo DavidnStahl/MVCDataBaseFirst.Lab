7bacaffd74f463a9b0aaf021f37c2fe8923b1c3b
47720480 <david.n.stahl@gmail.com>
2020-04-21 14:48:06 +0200

    first commit

---
+++ Models/Bil.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCDataBaseCodeFirst.LAB.Models
+{
+    public class Bil
+    {
+      
+
+        public int Id { get; set; }
+
+        public string Manufacturer { get; set; }
+
+        public string Model { get; set; }
+
+        public int Year { get; set; }
+
+        public decimal Price { get; set; }
+
+    }
+
+}
---
+++ Models/BilDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCDataBaseCodeFirst.LAB.Models
+{
+    public class BilDbContext : DbContext
+    {
+        public BilDbContext(DbContextOptions<BilDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Bil> Bilar { get; set; }
+    }
+}
